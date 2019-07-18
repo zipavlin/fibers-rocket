8a510af577f2664afa9ede266f465c60c7fd7056
Žiga Pavlin <ziga.pavlin@futura.si>
2019-07-18 10:05:57 +0200

    fix favicon for gh-pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,13 @@ module.exports = {
     base: '/fibers-rocket/',
     title: 'Fibers Rocket',
     description: 'Laravel development tool for rapid app development with smart model, migration, controller and views scaffolding.',
+    head: [
+        ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"}],
+        ['link', { rel: "manifest", href: "/site.webmanifest"}],
+        ['link', { rel: "shortcut icon", href: "/favicon.ico"}],
+    ],
     theme: '@vuepress/theme-default',
     plugins: [
         '@vuepress/search', { searchMaxSuggestions: 10 },
